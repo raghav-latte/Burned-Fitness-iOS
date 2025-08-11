191f9d6d766a885e66955b65aefde8a3a0a05bc7
raghav-latte <131696988+raghav-latte@users.noreply.github.com>
2025-08-11 11:27:40 +0530

    Initial Commit

---
+++ Burned/BurnedApp.swift
@@ -0,0 +1,17 @@
+//
+//  BurnedApp.swift
+//  Burned
+//
+//  Created by Raghav Sethi on 11/08/25.
+//
+
+import SwiftUI
+
+@main
+struct BurnedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
